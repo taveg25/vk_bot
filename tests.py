3c51d390a962340efdc3fa7769d5ec7b5ec21f85
Anatoliy Ter <tav-rezume@yandex.ru>
2021-11-02 07:15:37 +0300

    test function run

---
+++ tests.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import patch, Mock
+
+from bot import Bot
+
+
+class Test1(unittest.TestCase):
+    def test_ok(self):
+        count = 5
+        events = [{}] * count
+        long_poller_mock = Mock(return_value=events)
+        long_poller_listen_mock = Mock()
+        long_poller_listen_mock.listen = long_poller_mock
+        with patch('bot.vk_api.VkApi'):
+            with patch('bot.VkBotLongPoll', return_value=long_poller_listen_mock):
+                bot = Bot('','')
+                bot.on_event = Mock()
+                bot.run()
+
+                bot.on_event.assert_called()
+                bot.on_event.assert_any_call({})
+                assert bot.on_event.call_count == count
+
+
+
+if __name__ == '__main__':
+    unittest.main()
