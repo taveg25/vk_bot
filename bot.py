122a5b12d598aee7c0ce235286a2f0612a14e41b
Anatoliy Ter <tav-rezume@yandex.ru>
2021-12-14 05:52:05 +0300

    add database

--- bot.py
+++ bot.py
@@ -4,6 +4,12 @@
 
 import logging
 import random
+
+from pony.orm import db_session
+
+import handlers
+from models import UserState, Registration
+
 try:
     import settings
 except ImportError:
@@ -26,12 +32,17 @@ def configure_logging():
     log.addHandler(file_handler)
     log.setLevel(logging.DEBUG)
 
-
 class Bot(object):
     """
     Echo bot for vk.com
-
+    Сценарий регистрации на конференции через vk.com.
     Use python3.7
+
+    Поддерживает ответы на вопросы про дату, место проведения и сценарий регистрации:
+    - спрашиваем имя
+    - спрашиваем емейл
+    - говорим об успешной регистрации
+    Если шаг не пройдет, задаем уточняющий вопрос пока шаг не будет пройден.
     """
     def __init__(self, group_id, token):
         """
@@ -55,9 +66,10 @@ class Bot(object):
             # print('Send a message')
             try:
                 self.on_event(event)
-            except Exception as exc:
+            except Exception:
                 log.exception('ошибка в обработке события')
 
+    @db_session
     def on_event(self, event):
         """
         handle bots messages: sent our message back, if it's text
@@ -66,15 +78,65 @@ class Bot(object):
 
         """
 
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            log.debug('отправляем сообщение назад')
-            self.api.messages.send(message=f'было полученно сообщение: '
-                                           f"{event.object.message['text']}",
-                                   random_id=random.randint(0, 2 ** 20),
-                                   peer_id=event.object.message['peer_id'])
-        else:
+        if event.type != VkBotEventType.MESSAGE_NEW:
             log.info(f'Мы пока не умеем обрабатывать события такого типа {event.type}')
+            return
 
+        user_id = event.object.message['peer_id']
+        text = event.object.message['text']
+        state = UserState.get(user_id=str(user_id))
+
+        if state is not None:
+            text_to_send = self.continue_scenario(text, state)
+        else:
+            for intent in settings.INTENTS:
+                log.debug(f'User gets {intent}')
+                if any(token in text.lower() for token in intent['tokens']):
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+
+
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 20),
+            peer_id=user_id)
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        UserState(user_id=str(user_id), scenario_name=scenario_name, step_name=first_step, context={})
+        return text_to_send
+
+    def continue_scenario(self, text, state):
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            # next step
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                # switch to next step
+                state.step_name = step['next_step']
+            else:
+                # finish scenario
+                log.info('Registered {name} {email}'.format(**state.context))
+                Registration(name=state.context['name'], email=state.context['email'])
+                state.delete()
+
+        else:
+            # retry current step
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
 
 if __name__ == "__main__":
     configure_logging()
--- tests.py
+++ tests.py
@@ -1,9 +1,21 @@
 import unittest
+from copy import deepcopy
 from unittest.mock import patch, Mock, ANY
 
+from pony.orm import db_session, rollback
 from vk_api.bot_longpoll import VkBotMessageEvent
 
+import settings
 from bot import Bot
+
+
+def isolate_db(test_func):
+    def wrapper(*args, **kwargs):
+        with db_session:
+            test_func(*args, **kwargs)
+            rollback()
+    return wrapper
+
 
 
 class Test1(unittest.TestCase):
@@ -19,9 +31,10 @@ class Test1(unittest.TestCase):
              'keyboard': True, 'inline_keyboard': True, 'carousel': True, 'lang_id': 0}},
                  'group_id': 41621661, 'event_id': 'b4219a8adefedd28643ed50a60a52226fe0ad846'}
 
-    def test_ok(self):
+    def test_run(self):
         count = 5
-        events = [{}] * count
+        obj = {'a': 1}
+        events = [obj] * count
         long_poller_mock = Mock(return_value=events)
         long_poller_listen_mock = Mock()
         long_poller_listen_mock.listen = long_poller_mock
@@ -32,28 +45,55 @@ class Test1(unittest.TestCase):
                 bot.run()
 
                 bot.on_event.assert_called()
-                bot.on_event.assert_any_call({})
+                bot.on_event.assert_any_call(obj)
                 assert bot.on_event.call_count == count
 
+    INPUTS = [
+        'Привет',
+        'А когда?',
+        'Где будет конференция?',
+        'Зарегистрируй меня',
+        'Веня',
+        'мой адрес email@email',
+        'email@email.ru',
+    ]
+    EXPECTED_OUTPUTS = [
+        settings.DEFAULT_ANSWER,
+        settings.INTENTS[0]['answer'],
+        settings.INTENTS[1]['answer'],
+        settings.SCENARIOS['registration']['steps']['step1']['text'],
+        settings.SCENARIOS['registration']['steps']['step2']['text'],
+        settings.SCENARIOS['registration']['steps']['step2']['failure_text'],
+        settings.SCENARIOS['registration']['steps']['step3']['text'].format(name='Веня', email='email@email.ru'),
+    ]
 
-    def test_on_event(self):
-        event = VkBotMessageEvent(raw=self.RAW_EVENT)
-
+    @isolate_db
+    def test_run_ok(self):
         send_mock = Mock()
+        api_mock = Mock()
+        api_mock.messages.send = send_mock
 
-        with patch('bot.vk_api.VkApi'):
-            with patch('bot.VkBotLongPoll'):
-                bot = Bot('', '')
-                bot.api = Mock()
-                bot.api.messages.send = send_mock
+        events = []
+        for input_text in self.INPUTS:
+            event = deepcopy(self.RAW_EVENT)
+            event['object']['text'] = input_text
+            events.append(VkBotMessageEvent(event))
 
-                bot.on_event(event)
-        send_mock.assert_called_once_with(
-            message=f"было полученно сообщение: {self.RAW_EVENT['object']['message']['text']}",
-            random_id=ANY,
-            peer_id=self.RAW_EVENT['object']['message']['peer_id'],
-        )
+        long_poller_mock = Mock()
+        long_poller_mock.listen = Mock(return_value=events)
 
+        with patch('bot.VkBotLongPoll', return_value=long_poller_mock):
+            bot = Bot('','')
+            bot.api = api_mock
+            bot.run()
+
+        assert send_mock.call_count == len(self.INPUTS)
+
+        real_outputs = []
+        for call in send_mock.call_args_list:
+            args, kwargs = call
+            real_outputs.append(kwargs['message'])
+        assert real_outputs == self.EXPECTED_OUTPUTS
 
 
 if __name__ == '__main__':
